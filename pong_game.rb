91be977f5e55919122cce406b2052472d87c77ec
John Moon <johnmoonyy@gmail.com>
2013-11-16 14:05:56 -0500

    Created the red ball on the window

--- pong_game.rb
+++ pong_game.rb
@@ -10,6 +10,14 @@ class Pong < Hasu::Window
   def initialize
     super(WIDTH, HEIGHT, false)
   end
+  
+  def reset
+    @ball = Ball.new
+  end
+
+  def draw
+    @ball.draw(self)
+  end
 end
 
 Pong.run

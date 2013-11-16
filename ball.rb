7a3d98e32d20abe556135adfa0e3471abbe60244
John Moon <johnmoonyy@gmail.com>
2013-11-16 14:53:23 -0500

    Added scoring feature

--- ball.rb
+++ ball.rb
@@ -1,12 +1,40 @@
 class Ball
-  SIZE = 8 
+  SIZE = 16 
 
-  attr_reader :x, :y
+  attr_reader :x, :y, :angle, :speed
+
   def initialize
     @x = Pong::WIDTH/2
     @y = Pong::HEIGHT/2
+
+    @angle = rand(120) +30
+    @angle *= -1 if rand > 0.5 
+    @speed = 6 
+  end
+  
+  def  dx; Gosu.offset_x(angle, speed); end
+  def  dy; Gosu.offset_y(angle, speed); end
+  
+  def move!
+
+    @x += dx
+    @y += dy
+
+    if @y < 0 
+      @y = 0
+      bounce_off_edge!
+    end
+
+    if @y > Pong::HEIGHT 
+      @y = Pong::HEIGHT 
+      bounce_off_edge!
+    end
   end
 
+  def bounce_off_edge!
+    @angle = Gosu.angle(0, 0, dx, -dy)
+  end
+  
   def x1; @x - SIZE/2; end
   def x2; @x + SIZE/2; end
   def y1; @y - SIZE/2; end
@@ -21,5 +49,13 @@ class Ball
     x2, y1, color,
     x2, y2, color
     )
+  end
+
+  def off_left?
+  x1 < 0 
+  end
+
+  def off_right?
+  x2 > Pong::WIDTH
   end
 end
--- pong_game.rb
+++ pong_game.rb
@@ -2,6 +2,7 @@ require "bundler/setup"
 require "hasu"
 
 Hasu.load "ball.rb"
+Hasu.load "paddle.rb"
 
 class Pong < Hasu::Window
   WIDTH = 768
@@ -13,10 +14,37 @@ class Pong < Hasu::Window
   
   def reset
     @ball = Ball.new
+
+    @left_score = 0 
+    @right_score = 0 
+
+    @font = Gosu::Font.new(self, "Arial", 30) 
+
+    @left_paddle = Paddle.new(:left)
+    @right_paddle = Paddle.new(:right)
   end
 
   def draw
     @ball.draw(self)
+
+    @font.draw(@left_score, 30, 30, 0)
+    @font.draw(@right_score, WIDTH-50, 30, 0)
+
+    @left_paddle.draw(self)
+    @right_paddle.draw(self)
+  end
+
+  def update
+    @ball.move!
+
+    if @ball.off_left?
+      @left_score += 1
+      @ball = Ball.new
+    end
+    if @ball.off_right? 
+      @right_score += 1
+      @ball = Ball.new
+    end
   end
 end
 
